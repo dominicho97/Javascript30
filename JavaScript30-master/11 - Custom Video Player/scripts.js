2aded9e0630be6b4705a6297b3fa9f4fe0ddf835
dominicho97 <dominic.ho97@gmail.com>
2018-11-30 15:52:08 +0100

    added eventlisteners and function togglePlay, updateButton

--- JavaScript30-master/11 - Custom Video Player/scripts.js
+++ JavaScript30-master/11 - Custom Video Player/scripts.js
@@ -1,6 +1,12 @@
 // Get our elements
 
 const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelector('[data-skip]');
+const ranges = player.querySelector('.player-slider');
 
 
 
@@ -10,14 +16,24 @@ const player = document.querySelector('.player');
 
 //Build our functions
 
+function togglePlay(){ // als video=pauze-> afspelen of anders pauzeren
+  if (video.paused){
+    video.play(); 
+  } else {
+    video.pause();
+  }
+  }
+
+function updateButton(){
+  console.log('update the button');
+}
 
 
+/* Hook up event listeners*/
 
+video.addEventListener('click',togglePlay );
+video.addEventListener('play',updateButton );
+video.addEventListener('pause',updateButton );
 
+toggle.addEventListener('click',togglePlay );
 
-
-
-
-
-
-/* Hook up vent listeners*/
