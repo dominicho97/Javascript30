c2b439cb4f10d83e3d9b548596919f294bf12dd6
dominicho97 <dominic.ho97@gmail.com>
2018-12-12 11:47:19 +0100

    added const end,hour,minutes and endTime to function displayEndTime

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -25,7 +25,16 @@
   function displayTimeLeft(seconds){
   const minutes = Math.floor(seconds / 60);
   const remainderSeconds = seconds % 60;
-  const display = `${minutes}: ${remainderSeconds}`; // wat toont de display?
+  const display = `${minutes}: ${remainderSeconds < 10 ? '0': "" }${remainderSeconds}`; // wat toont de display?
+  document.title = display; // zorgt dat de titel vs webpagina ook een timer is
   timerDisplay.textContent = display;         // div van HTML
-    ;
+    
+  }
+
+  function displayEndTime(timeStamp){
+    const end = new Date(timeStamp);
+    const hours = end.getHours();
+    const minutes = end.getMinutes();
+    endTime = document.querySelector('.display__time-left');
+    endTime.textContent = `Be back at ${hour}:${minutes}`;
   }
