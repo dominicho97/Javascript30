b20da2e254e472dfafab5ad64395a1ed1ed44451
dominicho97 <dominic.ho97@gmail.com>
2018-12-12 11:03:00 +0100

    added math.round to const secondsLeft

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -1,5 +1,5 @@
 
-  let countdown:
+  let countdown;
   
   
   
@@ -8,10 +8,11 @@
   const now = Date.now()
   const then = now + seconds*1000;
   
-  setInterval(() => {
-    const secondsLeft = (then - Date.now()) / 1000;
+  countdown = setInterval(() => {
+    const secondsLeft = Math.round((then - Date.now()) / 1000);
     // check if we should stop it
-    if (secondsLeft <= 0){
+    if (secondsLeft < 0){
+      clearInterval(countdown);
       return;
     }
     //display it
