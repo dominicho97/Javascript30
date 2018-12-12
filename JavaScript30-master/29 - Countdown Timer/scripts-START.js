202f87e3e7759747255b8c0170ad8822a0708ab2
dominicho97 <dominic.ho97@gmail.com>
2018-12-12 10:58:47 +0100

    added function setInterval and let countdown

--- JavaScript30-master/29 - Countdown Timer/scripts-START.js
+++ JavaScript30-master/29 - Countdown Timer/scripts-START.js
@@ -1,6 +1,19 @@
 
-
-function(timer){
+  let countdown:
+  
+  
+  
+  
+  function timer(seconds){
   const now = Date.now()
-  const then = now + seconds;
-}
+  const then = now + seconds*1000;
+  
+  setInterval(() => {
+    const secondsLeft = (then - Date.now()) / 1000;
+    // check if we should stop it
+    if (secondsLeft <= 0){
+      return;
+    }
+    //display it
+    console.log(secondsLeft);
+  } , 1000)}
